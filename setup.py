303c3c8332a9f0ae54d41622a0f8d3cb1106b7d5
Daniel Ramsay <dr@circlesquares.co.uk>
2012-12-25 00:28:15 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+import os
+from setuptools import setup
+
+
+setup(
+	name = 'NORM',
+	version = '0.8.0',
+	author = 'Daniel Ramsay',
+	author_email = 'daniel@dretzq.org.uk',
+
+	license = 'BSD',
+	url = 'http://www.github.com/dantheta/norm',
+	packages = ['NORM'],
+	
+	description = 'NORM - not an ORM, a simple table mapper for Python.',
+	requires = ['psycopg2'],
+)
